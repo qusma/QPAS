01babcf60103c725d69a7d3a3a2010a2e9e3ba04
qusma <qusmablog@gmail.com>
2014-06-12 15:50:16 +0300

    Added an update button to the help menu.

--- QPAS/Windows/MainWindow.xaml.cs
+++ QPAS/Windows/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using NLog;
 using NLog.Targets;
 using Tag = EntityModel.Tag;
 
+
 namespace QPAS
 {
     /// <summary>
@@ -840,6 +841,74 @@ namespace QPAS
             }
 
             TradesRepository.Save();
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateCheckInfo info = null;
+
+            if (!ApplicationDeployment.IsNetworkDeployed) return;
+            ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+
+            try
+            {
+                info = ad.CheckForDetailedUpdate();
+
+            }
+            catch (DeploymentDownloadException dde)
+            {
+                System.Windows.Forms.MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
+                return;
+            }
+            catch (InvalidDeploymentException ide)
+            {
+                System.Windows.Forms.MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
+                return;
+            }
+            catch (InvalidOperationException ioe)
+            {
+                System.Windows.Forms.MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
+                return;
+            }
+
+            if (info.UpdateAvailable)
+            {
+                Boolean doUpdate = true;
+
+                if (!info.IsUpdateRequired)
+                {
+                    System.Windows.Forms.DialogResult dr = System.Windows.Forms.MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", System.Windows.Forms.MessageBoxButtons.OKCancel);
+                    if (System.Windows.Forms.DialogResult.OK != dr)
+                    {
+                        doUpdate = false;
+                    }
+                }
+                else
+                {
+                    // Display a message that the app MUST reboot. Display the minimum required version.
+                    System.Windows.Forms.MessageBox.Show("This application has detected a mandatory update from your current " +
+                                                         "version to version " + info.MinimumRequiredVersion.ToString() +
+                                                         ". The application will now install the update and restart.",
+                        "Update Available",
+                        System.Windows.Forms.MessageBoxButtons.OK,
+                        System.Windows.Forms.MessageBoxIcon.Information);
+                }
+
+                if (doUpdate)
+                {
+                    try
+                    {
+                        ad.Update();
+                        System.Windows.Forms.MessageBox.Show("The application has been upgraded, and will now restart.");
+                        System.Windows.Forms.Application.Restart();
+                    }
+                    catch (DeploymentDownloadException dde)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
+                        return;
+                    }
+                }
+            }
         }
     }
 }
