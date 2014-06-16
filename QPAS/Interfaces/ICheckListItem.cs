29787b0e4411ac0032dc9e55317712207372f3d5
qusma <qusmablog@gmail.com>
2014-06-16 14:37:04 +0300

    Added ICheckListItem interfaces.

---
+++ QPAS/Interfaces/ICheckListItem.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ICheckListItem.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.ComponentModel;
+
+namespace QPAS
+{
+    public interface ICheckListItem : INotifyPropertyChanged
+    {
+        object Tag { get; set; }
+        bool IsChecked { get; set; }
+    }
+
+    public interface ICheckListItem<T>
+    {
+        T Item { get; set; }
+    }
+}
