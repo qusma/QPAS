286012495c3b41f81d6b1192f66b1637619f8027
qusma <qusmablog@gmail.com>
2016-08-21 13:41:45 +0300

    Added some icons to trades

---
+++ QPAS/Converters/TradeToVisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="OrderToVisibilityConverter.cs" company="">
+// Copyright 2015 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using EntityModel;
+
+namespace QPAS
+{
+    [ValueConversion(typeof(Order), typeof(Visibility))]
+    public class TradeToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var trade = value as Trade;
+            if (trade == null) return Visibility.Hidden;
+            if (trade.Open) return Visibility.Hidden;
+            return Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value that is produced by the binding target.</param><param name="targetType">The type to convert to.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
