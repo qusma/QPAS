3c032b6ae25409355ac14c2a5149fd01d8289a29
qusma <qusmablog@gmail.com>
2016-08-21 13:40:08 +0300

    Some more classes to QPAS.Common

---
+++ QPAS.Common/ViewModels/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ViewModelBase.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace QPAS
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        protected IDialogService DialogService;
+
+        public virtual void Refresh()
+        {
+        }
+
+        public ViewModelBase(IDialogService dialogService = null)
+        {
+            DialogService = dialogService;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
